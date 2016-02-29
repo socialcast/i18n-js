edc7ed538472a152eafeec2b819bbf6a738bede3
Sean Walbran <swalbran@socialcast.com>
2016-02-28 20:04:47 -0600

    update for sprockets 3

--- lib/i18n/js/railtie.rb
+++ lib/i18n/js/railtie.rb
@@ -3,15 +3,21 @@ require 'i18n/js/engine'
 module I18n
   module Js
     autoload :Translator, "i18n/js/translator"
-    autoload :FileDependencyProcessor, "i18n/js/file_dependency_processor"
     autoload :StaticAssetRenderer, "i18n/js/static_asset_renderer"
 
     class Railtie < ::Rails::Railtie
       rake_tasks do
         load "tasks/i18n-js.rake"
       end
-      initializer "i18n-js.assetpipeline.environment", :after => "sprockets.environment" do |app|
-        app.assets.register_preprocessor("application/javascript", FileDependencyProcessor) if app.config.assets.enabled
+      initializer "i18n-js.register_preprocessor", after: :engines_blank_point, before: :finisher_hook do
+        Rails.application.config.assets.configure do |config|
+          config.register_preprocessor("application/javascript", :"i18n-js_dependencies") do |context, source|
+            if context.logical_path == "i18n/filtered"
+              ::I18n.load_path.each {|path| context.depend_on(File.expand_path(path))}
+            end
+            source
+          end
+        end
       end
     end
   end
