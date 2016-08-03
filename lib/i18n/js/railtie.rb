d9fe75372d99c20ea8dfcb2ff5a129e7da29fe6d
Sean Walbran <swalbran@socialcast.com>
2016-08-03 08:28:46 -0500

    improve asset change detection by using explicit dependencies in erb

--- lib/i18n/js/railtie.rb
+++ lib/i18n/js/railtie.rb
@@ -3,18 +3,11 @@ require 'i18n/js/engine'
 module I18n
   module Js
     autoload :Translator, "i18n/js/translator"
-    autoload :FileDependencyProcessor, "i18n/js/file_dependency_processor"
     autoload :StaticAssetRenderer, "i18n/js/static_asset_renderer"
 
     class Railtie < ::Rails::Railtie
       rake_tasks do
         load "tasks/i18n-js.rake"
-      end
-      initializer "i18n-js.assetpipeline.environment", :after => "sprockets.environment" do |app|
-        next unless app.config.assets.enabled
-        app.config.assets.configure do |config|
-          config.register_preprocessor('application/javascript', FileDependencyProcessor)
-        end
       end
     end
   end
